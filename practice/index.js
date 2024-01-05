0c293d5c79587a6ec60a6172964671b7ba8f5d09
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-01-05 16:00:53 +0000

    practice #1

--- practice/index.js
+++ practice/index.js
@@ -5,21 +5,33 @@
 // let todo3 = "swap phone";
 
 // console.log("It is a type of: "+typeof(todo1));
-let date = new Date();
-let getMonth = date.getMonth();
-let month;
 
-let dayOfMonth = date.getDay();
-let year = date.getFullYear();
+// let date = new Date();
+// let getMonth = date.getMonth();
+// let month;
+
+// let dayOfMonth = date.getDay();
+// let year = date.getFullYear();
 
 
-switch (getMonth) {
-    case 0:
-        month = "January";        
-        break;
+// switch (getMonth) {
+//     case 0:
+//         month = "January";        
+//         break;
 
-    default:
-        console.log("unknown month");
-        break;
-}
-console.log("Today's date is: "+month+ " "+ dayOfMonth + ", "+year);
+//     default:
+//         console.log("unknown month");
+//         break;
+// }
+// console.log("Today's date is: "+month+ " "+ dayOfMonth + ", "+year);
+
+// let observation1 = 4 + 5 * 3;
+// let observation2 = (4 + 5) * 3;
+
+// console.log(observation1);
+// console.log(observation2);
+
+
+let age = 20;
+let message = "I am " + age + " years old";
+console.log("The typeOf message variable is: "+ typeof(message));
