7c6a5b23713676d6eeca960271cae85d9e9f1e3f
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-01-05 19:08:39 +0000

    work in progress #8

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-todos = ["Get Pankcake", "Sell Sobolo","Credit Koosey" ]
+const todos = ["Get Pankcake", "Sell Sobolo","Credit Koosey" ]
 
-todos.forEach(todoHeading => {
-    let element = document.createElement('div');
-    element.innerText = todoHeading;
-    document.body.appendChild(element);  
-});
+render();
 
 function addToDo (){
-    let inputID = document.getElementById('todo-item');
-    let inputText = inputID.value;
+    const inputID = document.getElementById('todo-item');
+    const inputText = inputID.value;
     todos.push(inputText);
+    console.log(todos);
+
+    render();
+}
+
+function render(){
+    // reset the display list after each array update
+    document.getElementById('todo-list').innerHTML = '';
+
+todos.forEach(todoHeading => {
+    const element = document.createElement('div');
+    element.innerText = todoHeading;
+    let todoListID = document.getElementById('todo-list')
+    todoListID.appendChild(element);  
+});
 }
