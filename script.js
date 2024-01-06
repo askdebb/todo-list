3d79dffe88be7fba13ca6249cde0365395df21bc
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-01-06 15:47:53 +0000

    work in progress #11(real)

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const todos = ["Get Pankcake", "Sell Sobolo","Credit Koosey" ]
+const todos = [{
+        title:"Get Pankcake",
+        dueDate:"2024-03-12"
+    }, 
+    {
+        title: "Sell Sobolo",
+        dueDate: "2024-01-02"
+    },
+    {
+        title: "Credit Koosey",
+        dueDate: "2024-12-23"
+    } ]
 
 render();
 
 function addToDo (){
     const inputID = document.getElementById('todo-item');
     const inputText = inputID.value;
-    todos.push(inputText);
+
+    const dateInputID = document.getElementById('todo-date')
+    const dateInputInfo = dateInputID.value;
+    todos.push({
+        title: inputText,
+        dueDate: dateInputInfo
+    });
     console.log(todos);
 
     render();
@@ -15,9 +32,9 @@ function render(){
     // reset the display list after each array update
     document.getElementById('todo-list').innerHTML = '';
 
-todos.forEach(todoHeading => {
+todos.forEach(todoObjectItems => {
     const element = document.createElement('div');
-    element.innerText = todoHeading;
+    element.innerText = todoObjectItems.title + ' '+todoObjectItems.dueDate;
     let todoListID = document.getElementById('todo-list')
     todoListID.appendChild(element);  
 });
