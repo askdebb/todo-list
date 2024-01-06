dd7415a4ceeb23081953f9849105bbfa9689a2a2
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-01-06 21:30:03 +0000

    work in progress #14(real)

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const todos = [{
         title:"Get Pankcake",
-        dueDate:"2024-03-12"
+        dueDate:"2024-03-12",
+        id: 'id1'
     }, 
     {
         title: "Sell Sobolo",
-        dueDate: "2024-01-02"
+        dueDate: "2024-01-02",
+        id:'id2'
     },
     {
         title: "Credit Koosey",
-        dueDate: "2024-12-23"
+        dueDate: "2024-12-23",
+        id:'id3'
     } ]
 
 render();
@@ -19,9 +22,12 @@ function addToDo (){
 
     const dateInputID = document.getElementById('todo-date')
     const dateInputInfo = dateInputID.value;
+
+    const id = new Date().getTime();
     todos.push({
         title: inputText,
-        dueDate: dateInputInfo
+        dueDate: dateInputInfo,
+        id: id
     });
     console.log(todos);
 
@@ -34,8 +40,23 @@ function render(){
 
 todos.forEach(todoObjectItems => {
     const element = document.createElement('div');
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.style = 'margin-left: 30px';
+    deleteBtn.id = todoObjectItems.id;
+    deleteBtn.onclick = deleteTodo;
+
     element.innerText = todoObjectItems.title + ' '+todoObjectItems.dueDate;
+    element.appendChild(deleteBtn);
+
     let todoListID = document.getElementById('todo-list')
     todoListID.appendChild(element);  
 });
+}
+
+function deleteTodo (e){
+    const delBtn = e.target;
+    const delBtnID = delBtn.id;
+    console.log(delBtnID);
 }
